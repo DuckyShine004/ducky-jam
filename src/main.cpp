7cb6dac366386366ef0edb77b498230fb2db00ae
Gallon Zhou <gallonzhou04@gmail.com>
2025-06-16 00:27:55 +1200

    Feat: Added Application

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,13 @@
-#include <external/glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <engine/shader/Shader.hpp>
+#include <application/Application.hpp>
+
+using namespace application;
 
 int main() {
-    glfwInit();
+    Application application;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    application.initialise();
 
-    GLFWwindow *window = glfwCreateWindow(1080, 720, "Ducky Jam", nullptr, nullptr);
-
-    if (window == nullptr) {
-        std::cout << "Failed to create window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialise GLAD" << std::endl;
-
-        return -1;
-    }
-
-    engine::shader::Shader shader("shaders/game/singleplayer/play/play");
-    shader.initialise();
-
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
+    application.run();
 
     return 0;
 }
